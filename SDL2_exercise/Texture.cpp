7e5d918cbbb5f7abaf9f65daf86a062c3a829564
Ferre-spec <ceustermansferre@gmail.com>
2024-12-09 12:47:02 +0000

    changes Opengl texture loading

--- SDL2_exercise/Texture.cpp
+++ SDL2_exercise/Texture.cpp
@@ -2,27 +2,7 @@
 #include"Texture.h"
 #include<vector>
 #include <iostream>
-
-//
-//SDL_Texture* LoadTexture(std::string filePath, SDL_Renderer* renderTarget)
-//{
-//	SDL_Texture* texture = nullptr;
-//	SDL_Surface* surface = SDL_LoadBMP(filePath.c_str());
-//	if (surface == NULL)
-//		std::cout << "Error" << std::endl;
-//	else
-//	{
-//		SDL_SetColorKey(surface, SDL_TRUE, SDL_MapRGB(surface->format, 255, 0, 255));
-//		texture = SDL_CreateTextureFromSurface(renderTarget, surface);
-//		if (texture == NULL)
-//			std::cout << "Error" << std::endl;
-//	}
-//
-//	SDL_FreeSurface(surface);
-//
-//	return texture;
-//}
-
+#include<glad/glad.h>
 
 Texture::~Texture()
 {
@@ -32,17 +12,183 @@ Texture::~Texture()
 
 Texture::Texture()
 {
+	float vertices[] = {
+		// positions         // colors           // texture coords
+		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+	   -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+	   -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
+	};
 
+	unsigned int indices[] = {  // note that we start from 0!
+	0, 1, 3,   // first triangle
+	1, 2, 3    // second triangle
+	};
 	//LoadTexture(background_file_path, );
 
+	GLuint vbo; // vertex buffer object
+	glGenBuffers(1, &vbo); // Generate 1 buffer
+
+	GLuint ebo;
+	glGenBuffers(1, &ebo);
+
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+
+	// 1. bind Vertex Array Object
+	glBindVertexArray(vao);
+
+	// 2. copy our vertices array in a buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	const char* vertexShaderSource = R"glsl(
+		#version 330 core
+
+		in vec3 position;
+		in vec3 color;
+		in vec2 texCoord;
+		
+		out vec3 Color;
+		out vec2 TexCoord;
+
+		void main()
+		{
+			Color = color;
+			TexCoord = texCoord;
+			gl_Position = vec4(position, 1.0);
+
+		}
+		)glsl";
+
+
+	// Vertex Shader
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLint  success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// Fragment Shader
+
+	const char* fragmentShaderSource = R"glsl(
+		#version 330 core
+		in vec3 Color;
+		in vec2 TexCoord;
+
+		out vec4 outColor;
+
+		uniform sampler2D ourTexture;
+		uniform sampler2D ourTexture2;
+
+		void main()
+		{
+			vec4 colTex1 = texture(ourTexture, TexCoord);
+			vec4 colTex2 = texture(ourTexture2, TexCoord);
+			outColor = mix(colTex1, colTex2, 0.5);
+		})glsl";
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	GLuint shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// 3. then set our vertex attributes pointers
+	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+	glEnableVertexAttribArray(posAttrib);
+	glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+
+	GLint colorAttrib = glGetAttribLocation(shaderProgram, "color");
+	glEnableVertexAttribArray(colorAttrib);
+	glVertexAttribPointer(colorAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+
+	GLint texCoordAttrib = glGetAttribLocation(shaderProgram, "texCoord");
+	glEnableVertexAttribArray(texCoordAttrib);
+	glVertexAttribPointer(texCoordAttrib, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+
+
+
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+
+	// set the texture wrapping/filtering options (on the currently bound texture object)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	stbi_set_flip_vertically_on_load(true);
+
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("galaxy2.bmp", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "OH NOOOOOO TEXTURE AINT LOADING" << std::endl;
+	}
+	stbi_image_free(data);
+
+
 }
 
-void forLoadTexture(std::string textureName)
+void Texture :: textureUpdate()
 {
-	//LoadTexture(textureName, wantSurface);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(shaderProgram);
+	glBindVertexArray(vao);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
-void textureRender()
+
+
+void Texture :: deleteTexture()
 {
-	//SDL_RenderCopy()
+
+
 }
